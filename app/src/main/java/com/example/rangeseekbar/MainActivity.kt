4335709c3b7963e66f17cb81ad5f56dee6afef04
Md Mehedi Hasan <mhshobuj37@gmail.com>
2021-05-26 17:35:48 +0600

    only design part there are available

---
+++ app/src/main/java/com/example/rangeseekbar/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.rangeseekbar
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.jem.rubberpicker.RubberRangePicker
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rubberRangePickerStartValue = findViewById<TextView>(R.id.rubberRangePickerStartValue)
+        val rubberRangePickerEndValue = findViewById<TextView>(R.id.rubberRangePickerEndValue)
+
+        val rubberRangePicker = RubberRangePicker(this)
+        rubberRangePicker.setMin(10)
+        rubberRangePicker.setMax(865)
+
+
+//        val startThumbValue = rubberRangePicker.getCurrentStartValue()
+//        rubberRangePicker.setCurrentStartValue(startThumbValue + 10)
+//        val endThumbValue = rubberRangePicker.getCurrentEndValue()
+//        rubberRangePicker.setCurrentEndValue(endThumbValue + 10)
+//        rubberRangePicker.setOnRubberRangePickerChangeListener(object: RubberRangePicker.OnRubberRangePickerChangeListener{
+//            override fun onProgressChanged(rangePicker: RubberRangePicker, startValue: Int, endValue: Int, fromUser: Boolean) {}
+//            override fun onStartTrackingTouch(rangePicker: RubberRangePicker, isStartThumb: Boolean) {}
+//            override fun onStopTrackingTouch(rangePicker: RubberRangePicker, isStartThumb: Boolean) {}
+//        })
+
+        rubberRangePicker.setOnRubberRangePickerChangeListener(object: RubberRangePicker.OnRubberRangePickerChangeListener{
+            override fun onProgressChanged(rangePicker: RubberRangePicker, startValue: Int, endValue: Int, fromUser: Boolean) {
+                rubberRangePickerStartValue.text = startValue.toString()
+                rubberRangePickerEndValue.text = endValue.toString()
+            }
+            override fun onStartTrackingTouch(rangePicker: RubberRangePicker, isStartThumb: Boolean) {}
+            override fun onStopTrackingTouch(rangePicker: RubberRangePicker, isStartThumb: Boolean) {}
+        })
+    }
+}
